de138c020a1f177321dcf619bcd06116a7b2904a
ANAS MUFTI <muftianas97@gmail.com>
2025-03-07 14:39:09 +0500

    ...

--- base/urls.py
+++ base/urls.py
@@ -5,10 +5,12 @@ from .import  views
 
 urlpatterns  =[
 
-path('', views.home,
+path('',
+      views.home,
       name = "home" ),
 
-path('about/', views.about, name 
+path('about/',
+      views.about, name 
      = "about"),
 
 ]
